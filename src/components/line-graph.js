c3f7cd1c045747ad4a240e879ea3b5feb39fae98
Cristian Colorado <ccoloradoc@gmail.com>
2023-04-08 15:35:40 -0600

    Graph | Render when data available

--- src/components/line-graph.js
+++ src/components/line-graph.js
@@ -13,6 +13,8 @@ import {
 import { Line } from "react-chartjs-2";
 import moment from "moment-timezone"
 
+moment.locale('es-mx');
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -25,76 +27,90 @@ ChartJS.register(
 );
 
 export const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    interaction: {
-        intersect: false,
-        axis: 'x'
+  responsive: true,
+  maintainAspectRatio: false,
+  interaction: {
+    intersect: false,
+    axis: 'x'
+  },
+  plugins: {
+    legend: {
+      display: false,
     },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: false,
-      },
+    title: {
+      display: false,
     },
-    elements: {
-      line: {
-        tension: 0,
-        borderWidth: 2,
-        borderColor: "#12b5d2",
-        fill: "start",
-        backgroundColor: "#bbf0f3",
-      },
-      point: {
-        radius: 2,
-        hitRadius: 5,
-      },
+  },
+  elements: {
+    line: {
+      tension: 0,
+      borderWidth: 2,
+      borderColor: "#12b5d2",
+      fill: "start",
+      backgroundColor: "#bbf0f3",
     },
-    scales: {
-      y: {
-        display: false,
-      },
-      x: {
+    point: {
+      radius: 2,
+      hitRadius: 5,
+    },
+  },
+  scales: {
+    y: {
+      display: false,
+    },
+    x: {
+      display: true,
+      ticks: {
         display: true,
-        ticks: {
-          display: true,
-        },
-        grid: {
-          display: false,
-        },
+      },
+      grid: {
+        display: false,
       },
     },
-  };
+  },
+};
 
-const mockLabels = ["January", "February", "March", "April", "May", "June", "July", "AU"];
-export const mockData = [10, 15, 20, 12, 24, 17, 15,22]
+const mockLabels = ["A", "B"];
+export const mockData = [10, 15, 20, 12, 24, 17, 15, 22]
 
-export default function LineGraph({ id, historical }) {
+export default function LineGraph({ id, price, historical }) {
+  if (historical && historical.length > 1) {
     let data = []
     let labels = []
-    if (historical && historical.length > 1) {
-        historical.forEach(element => {
-            labels.push(moment(element.date).tz('America/Mexico_City').format("DD/MMMM HH:mm"))
-            data.push(element.value)
-        });
-    } else {
-        labels = mockLabels
-        data = mockData
-    }
+    // let previous = {
+    //   value: historical[0].value,
+    //   date: moment(historical[0].date)
+    // }
+    // labels.push(previous.date.tz('America/Mexico_City').format("DD/MMMM HH:mm"))
+    // data.push(previous.value)
+
+    historical.forEach(element => {
+
+      // if (previous.value == element.value && labels.length > 1) {
+      //   labels.pop()
+      //   data.pop()
+      // }
+
+      labels.push(moment(element.date).tz('America/Mexico_City').format("DD/MMMM HH:mm"))
+      data.push(element.value)
+    });
 
     let dataParam = {
-        labels,
-        datasets: [{
-            data: data,
-            stepped: true,
-        }]
+      labels,
+      datasets: [{
+        data: data,
+        stepped: true,
+      }]
     };
 
     return (
-        <div className="w-full h-24 mt-10 divide-y divide-blue-200">
-            <Line id={id} options={options} data={dataParam} />
-        </div>
+      <div className="w-full h-24 mt-10 divide-y divide-blue-200">
+        <Line id={id} options={options} data={dataParam} />
+      </div>
     )
+  } else {
+    return (
+      <></>
+    )
+  }
 }
